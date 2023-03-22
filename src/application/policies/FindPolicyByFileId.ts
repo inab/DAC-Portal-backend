1d0e73ecdd887b3a3cf0fb6f84a7b4c6136cbd8d
acavalls <alejandro.canosa@bsc.es>
2023-03-22 11:15:37 +0100

    Adding missing class def: FindPolicyByFileId

---
+++ src/application/policies/FindPolicyByFileId.ts
@@ -0,0 +1,16 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { configToken } from '../../infrastructure/config/env.config';
+import { IPoliciesRepository, IPoliciesRepositoryToken } from '../../domain/ports/repositories/policies/policies.interfaces';
+
+@Injectable()
+export class FindPolicyByFileId {
+  constructor(
+    @Inject(IPoliciesRepositoryToken) private readonly policiesRepository: IPoliciesRepository,
+    @Inject(configToken) private readonly configService: ConfigService,
+  ) {}
+
+  async run(id: string) {
+    return this.policiesRepository.findPolicyByFileId(id);
+  }
+}
